fa9b418a9ea0a1c9ce9773c83d4aa19e904bae8e
Rico Woods <ricowoods@Joes-MacBook-Pro-2.local>
2020-01-27 15:56:39 -0700

    completed

---
+++ Part2/index.js
@@ -0,0 +1,10 @@
+const id = document.getElementById("idInput");
+const inputColor = document.getElementById("inputColor");
+
+
+function setCard() {
+  
+    const card = document.getElementById(idInput.value);
+   card.style.color = inputColor.value
+   
+}
